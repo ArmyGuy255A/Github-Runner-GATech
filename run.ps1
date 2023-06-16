639fd91c38cd25f9d9465b9595a0664b3a623d25
Phil Dieppa <mrdieppa@gmail.com>
2023-06-17 09:20:39 +1000

    Added a comment

--- run.ps1
+++ run.ps1
@@ -1,1 +1,5 @@
-docker run -e GH_PAT='' -e GH_OWNER='' -e GH_REPOSITORY='' github-runner-gatech:latest
+# Note.. these are old PATs. They're here as an example...
+docker run -e GH_PAT='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' -e GH_OWNER='pdieppa3' -e GH_REPOSITORY='Github-Runner' --name github-runner-gatech armyguy255a/github-runner-gatech:latest
+
+# Debug
+docker run --entrypoint /bin/bash -it --rm -e GH_PAT='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' -e GH_OWNER='pdieppa3' -e GH_REPOSITORY='Github-Runner' --name github-runner-gatech armyguy255a/github-runner-gatech:latest
